be259b5e29bd606412485f0ba7c868ad0b14cecf
hussain2023 <iamhussainali2@gmail.com>
2022-10-01 10:11:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is heading</h1>
+      <h3>this is not a heading</h3>
+    </div>
+  );
+}
+
+export default App;
