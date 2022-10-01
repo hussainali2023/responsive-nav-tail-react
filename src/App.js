e30dda7815d3840e690c81db2cb9285fe88f4fc2
hussain2023 <iamhussainali2@gmail.com>
2022-10-01 10:18:35 +0530

    install tailwind and aded heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>This is heading</h1>
+      <h1 className="text-5xl font-extrabold bg-black text-white">
+        This is heading
+      </h1>
       <h3>this is not a heading</h3>
     </div>
   );
